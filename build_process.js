93e96a93fa5b67038324af339d6f98f413b0a61d
P3nny <p3nny@hacking.studio>
2020-01-07 23:10:09 +0100

    🔥 Remove next and add build process without framework

--- build_process.js
+++ build_process.js
@@ -1,27 +1,36 @@
 
-const { readdirSync } = require('fs')
+const { readdirSync, writeFile } = require('fs')
 
-const directories = readdirSync(__dirname+'/pages', { withFileTypes: true })
-    .filter(dirent => dirent.isDirectory())
+const directories = readdirSync(__dirname, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory() && dirent.name!=='.git')
     .map(dirent => dirent.name)
 
-let text = ''
+let text = `<!DOCTYPE html>
+<html>
+<body>
+<h1>Worthandlung</h1>
+<p>Code snippets</p>\n`
 
 for (const folder of directories) {
-    text+=`<h3>${folder}</h3>\n`;
-    const files = readdirSync(__dirname+'/pages/'+folder, { withFileTypes: true })
+    text+=`<h3>${folder}</h3>\n<ul>`;
+    const files = readdirSync(__dirname+'/'+folder, { withFileTypes: true })
     .filter(dirent => dirent.name.endsWith('.html'))
     .map(dirent => dirent.name)
     for (const file of files) {
-        text+=`<a href>${file}</a>\n`
+        text+=`<li><a href='/${folder}/${file}'>${file}</a></li>\n`
     }
+    text+=`</ul>`
 }
 
-console.log(text);
+text = text+`
+</body>
+</html>
+`
 
-
-
-// for each folder in pages:
-//     take folder name and use it as headline
-//     for each file in folder:
-//     take each file and use it as a link to this file
+writeFile('index.html', text, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Yay')
+    }
+  })
